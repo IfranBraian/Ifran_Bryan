fafa5990770e8e4c7d40ea3b45fad99c2a0831e3
Bryan Ifrán <ifranbraian16@gmail.com>
2022-11-30 19:20:49 +0100

    Ejercicios

---
+++ Modulo04/TP5/TP5ejer2.sql
@@ -0,0 +1,2 @@
+-- 1. Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar ordenados por la fecha de realización, mostrando en primer lugar los pedidos más recientes.
+SELECT * FROM ventas.pedidos ORDER BY pedidos.fecha DESC;
